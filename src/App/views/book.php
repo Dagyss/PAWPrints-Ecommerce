2bee155b85aa33292c476bad677cc6a50daa11eb
pfrovdev <pfrov.dev@gmail.com>
2025-05-05 01:56:14 -0300

    Refactorizamos book.php

--- src/App/views/book.php
+++ src/App/views/book.php
@@ -7,78 +7,70 @@
     <meta name="description" content="Libros en venta en PAWPrints. Consulta los libros disponibles en PAWPrints." />
     <meta name="keywords" content="PAWPrints, compra, libros, ebooks, productos" />
     <meta name="author" content="PAWPrints" />
-    <link rel="stylesheet" href="./styles/book-information.css" />
+    <link rel="stylesheet" href="./css/book-information.css" />
     <title>Libros - PAWPrints</title>
 </head>
 
 <body>
-
     <?php
-    require "parts/header.php";
+        require "parts/header.php";
     ?>
-
     <main>
+
         <nav aria-label="breadcrumb">
             <ul>
-                <li><a href="./index.html">Home</a></li>
-                <li><a href="./books.html">Libros</a></li>
+                <li><a href="./">Home</a></li>
+                <li><a href="./books">Libros</a></li>
                 <li><span>Libro</span></li>
             </ul>
         </nav>
-
         <h2>Información</h2>
-        <section class="flex-container">
+        <section class="content">
+            <section class="book-image">
             <img src="<?= htmlspecialchars($book->fields['imagen']) ?>" alt="Portada del Libro" />
+            </section>
             <section>
-                <h2><?= htmlspecialchars($book->fields['titulo']) ?></h2>
-                <p><?= htmlspecialchars($book->fields['autor']) ?></p>
-                <p>$<?= number_format($book->fields['precio'], 2, ',', '.') ?></p>
-                <p><?= htmlspecialchars($book->fields['id']) ?></p>
-
+                <h3 class="book-title"><?= htmlspecialchars($book->fields['titulo']) ?></h3>
+                <p class="book-author"><?= htmlspecialchars($book->fields['autor']) ?></p>
+                <p class="book-price">Precio: $<?= number_format($book->fields['precio'], 2, ',', '.') ?></p>
                 <fieldset>
                     <legend>Elige el formato:</legend>
-
-                    <input type="radio" id="fisico" name="formato-libro" value="f" checked="checked" />
+                
+                    <input type="radio" id="fisico" name="formato-libro" value="f" checked="checked"/>
                     <label for="fisico">Fisico</label>
-
+                
                     <input type="radio" id="electronico" name="formato-libro" value="e" />
                     <label for="electronico">Electronico</label>
                 </fieldset>
-                <input type="number" id="cantidad" name="cantidad" placeholder="1" min="1" />
-                <input type="button" value="Al carrito" />
+                <input type="number" id="cantidad" name="cantidad"  class="book-quantity" placeholder="1" min="1"/>
+    
+                <input type="button" class="check-button" value="Al carrito" />
+    
             </section>
-
-            <section>
-                <h2>descripcion</h2>
-                <p>Formato:LIBROS</p>
-                <p>Editorial:<?= htmlspecialchars($book->fields['editorial']) ?></p>
-                <p>Idioma:Español</p>
-                <p>ISBN:9789878121383</p>
-                <p>N° Páginas: 496</p>
-                <p>Fecha Publicación: xx/xxxx</p>
+            <section class="book-description">
+                <h3>Descripcion</h3>
+                <ul>
+                    <li>Formato: LIBROS</li>
+                    <li>Editorial: <?= htmlspecialchars($book->fields['editorial']) ?></li>
+                    <li>Idioma: Español</li>
+                    <li>ISBN: <?= htmlspecialchars($book->fields['isbn']) ?></li>
+                    <li>N° Páginas: <?=$book->fields['nro_paginas']?></li>
+                    <li>Fecha Publicación: <?= date_format($book->fields['fecha_publicacion'],'d/m/Y')?></li>
+                </ul>
             </section>
-
-            <section>
-                <h2>Sinopsis</h2>
-                <p><?= htmlspecialchars($book->fields['descripcion']) ?></p>
+            <section class="book-sinopsis" >
+                <h3>Sinopsis</h3>
+                <p><?= htmlspecialchars($book->fields['descripcion'])?></p>
+    
             </section>
+    </section>
 
-            <input type="radio" id="fisico" name="formato-libro" value="f" checked="checked" />
-            <label for="fisico">Fisico</label>
-
-            <input type="radio" id="electronico" name="formato-libro" value="e" />
-            <label for="electronico">Electronico</label>
-
-            </fieldset>
-            <input type="number" id="cantidad" name="cantidad" placeholder="1" min="1" />
-            <input type="button" value="Al carrito" />
-        </section>
     </main>
 
     <?php
-    require "parts/footer.php";
+        require "parts/footer.php";
     ?>
-
+    
 </body>
 
 </html>
